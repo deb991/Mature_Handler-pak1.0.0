29753e840c68cb685832550fa45bea7d3ba3b34b
Ozzius <deb991@users.noreply.github.com>
2016-04-09 15:53:23 +0530

    Cryptography_script_tab_1.0.0.sh
    This file is for allocation the whole operation based file transactions. Requesting to edit it in a live system not a running system.

---
+++ Cryptography_script_tab_1.0.0.sh
@@ -0,0 +1,43 @@
+/* In this file the allocated information for the Encryption are given. This is not suppose to execute by user prefference. */
+/* The program files will automatically execute this program when the allocation & access is needed. Once use this file r, edit after */
+/* install the encryption, may affect the whole process. Its better to use for test purpose in a LIVE system rather than host system. */
+/* This file content the aspected value for a specific architecture for this Cryptograpy. */
+/* APHOS: as Armoured Pure Heart Operating System framework based implementation. */
+
+#!/usr/bin
+#usage:make file for pbencryption system.
+
+#Proparty of:      #     ######   #     #  #######   #####   
+		#     #  #     #  #     #  #     #  #     #  
+	 	#     #  #     #  #     #  #     #  #        
+		#     #  ######   #######  #     #   #####   
+		#######  #        #     #  #     #        #  
+		#     #  #        #     #  #     #  #     #  
+		#     #  #        #     #  #######   #####
+
+#This program needs root previledgeto be executed & for security reason.
+cd '/' &&
+mkdir -v -p .APHOS_crypt
+cd /.APHOS_crypt &&
+mkdir -v .main\ files
+echo 'main files which are encrypted for security reason stored in this directory'
+COMMENT=main files for pbencryption system
+cd /.APHOS_crypt &&
+mkdir -v .patches
+echo 'All patches will be created & stored into this directory'
+COMMENT=patches dir contents all patch files
+cd /.APHOS_crypt &&
+mkdir -v .config
+echo 'configaration files of this encryption system will be stored into this directory'
+COMMENT=configuration files for further development and current configuration will be here
+cd /.APHOS_crypt &&
+mkdir -v .tester
+echo 'testing module include development of this program & further performence will be here'
+COMMENT=testing module for pbencryption system will be stored into this dir
+cd /.APHOS_crypt &&
+mkdir -v .About
+echo 'All development releted details & near possibilities can be found into this directory'
+cd /.APHOS_crypt &&
+mkdir -v .lib
+echo 'All lib files releted with this encryption system will be stored into this directory'
+COMMENT=lib contents of pbencryption system
